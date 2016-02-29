afb440a3086309a449ffc9d20801377e01110155
carolglanzmann <carolinecavalcante@Macintosh-2.local>
2016-02-29 14:25:06 -0800

    ADDED TEXT SCORE/LIVES ON SCREEN AND ADDED STAR COLLECTIBLE OBJECT

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-// Enemies our player must avoid
-var water = false;
+ // Enemies our player must avoid
+var water = false; // set initial player to the grass point (equals to not be in water)
 
 var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
@@ -42,6 +42,8 @@ var Player = function() {
     this.x = 200;
     this.y = 400;
     this.sprite = 'images/char-boy.png';
+    this.score = 0;
+    this.lives = 5;
 
 };
 
@@ -51,21 +53,41 @@ var resetGame = function(){
     player.y = 400;
 }
 
+//Update the status of player throughout the gameplay
 Player.prototype.update = function(dt) {
     this.enemyCollision();
     this.reachingWater();
+    this.showText();
 
-    
+
 };
 
+//Show text on screen for user lives and score
+Player.prototype.showText = function() {
+    ctx.clearRect(0,0,80,200);
+    ctx.clearRect(400,0,80,200);
+   
 
+    ctx.font = " 20px Impact";
+    ctx.fillStyle = "orange";
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1.5;
+
+    ctx.fillText("Score: " + this.score, 10, 35);
+    ctx.strokeText("Score: " + this.score, 10, 35);
+    ctx.fillText("Lives: " + this.lives, 420, 35);
+    ctx.strokeText("Lives: " + this.lives, 420, 35);
+
+};
+
+// Draw the player on the screen
 Player.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Added the function that handles the user input to control the player
-Player.prototype.handleInput = function(allowedKeys){
-    switch(allowedKeys) {
+Player.prototype.handleInput = function(inputKeys){
+    switch(inputKeys) {
         case 'left' :
         if(this.x - 101 < 0) {
             this.x = 0;
@@ -111,12 +133,19 @@ Player.prototype.handleInput = function(allowedKeys){
     }
 };
 
+
+
 // Set up collision when player interact with ladybug, thus reseting the game
 Player.prototype.enemyCollision = function (){
     var ladyBug = checkCollisions(allEnemies);
 
     if(ladyBug){
-        resetGame();
+        if(this.lives !== 0){
+            this.lives--;
+            resetGame();
+
+        }
+
     }
 
 };
@@ -129,25 +158,65 @@ Player.prototype.reachingWater = function(){
     }
 };
 
-// Create a function that returns the enemyArray to check for collisions
-var checkCollisions = function(enemyArray){
-    for (var i = 0; i < enemyArray.length; i++ ){
-        if(player.x < enemyArray[i].x + 50 &&
-            player.x + 50 > enemyArray[i].x &&
-            player.y < enemyArray[i].y + 40 &&
-            player.y + 40 > enemyArray[i].y){
-                return enemyArray[i];
+// Create a function that returns the enemyArray/ starsArray to check 
+//for collisions/collection when the function is called on the 
+//Enemy and Star instances respectivelly
+var checkCollisions = function(someArray){
+    for (var i = 0; i < someArray.length; i++ ){
+        if(player.x < someArray[i].x + 50 &&
+            player.x + 50 > someArray[i].x &&
+            player.y < someArray[i].y + 40 &&
+            player.y + 40 > someArray[i].y){
+                return someArray[i];
         }
     }
 };
 
+// Stars - object for player to collect during game
+var Star = function(){   
+    this.sprite = 'images/Star.png';
+    this.x = Math.floor(Math.random() * (7 - 1) + 1) * 101;
+    this.y = Math.floor(Math.random () * ( 3 - 1) + 1) * 80;
+
+};
+
+// Draw the stars on the screen
+Star.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+};
+
+// Set up for Collecting Stars/Points 
+Star.prototype.collectStars = function(){
+    var collectables = checkCollisions(allStars);
+    var index = allStars.indexOf(collectables);
+
+    if(index > -1) {
+        allStars.splice(index, 1);
+        player.score += 5; // Add 5 points for every star collected
+    }
+};
+
+// Update of Star collection 
+Star.prototype.update = function(){
+    this.collectStars();
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-var allEnemies = [new Enemy(90,50), new Enemy(75,224), new Enemy(154,139) ];
+var allEnemies = [new Enemy(90,50), new Enemy(75,224), new Enemy(154,139)];
 
 // Place the player object in a variable called player
 var player = new Player();
+
+
+// Instanciate the Star objects for player to collect
+var allStars = [];
+for (var i = 0; i < 4; i++){
+    var star =  new Star();
+    allStars.push(star);
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
